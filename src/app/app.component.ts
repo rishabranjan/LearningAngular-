e5c5ca927c580df3fb566d99291d37769d1b4bc7
Rishab Ranjan <therishabranjan@gmail.com>
2022-06-19 22:26:40 +0530

    created todo components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'LearningAngular';
+  allTodos: any = [
+    {
+      heading: 'Make Eggs',
+      desc: 'Make omlet from 4 eggs',
+    },
+    {
+      heading: 'Buy Eggs',
+      desc: 'Make omlet from 4 eggs',
+    },
+    {
+      heading: 'Sell Eggs',
+      desc: 'Make omlet from 4 eggs',
+    },
+    {
+      heading: 'Throw Eggs',
+      desc: 'Make omlet from 4 eggs',
+    },
+  ];
+  todoDesc: string = '';
+  todoHeading: string = '';
+
+  onTodoClick() {
+    if (this.todoHeading!=="" && this.todoDesc!=="") {
+      this.allTodos.push({"heading":this.todoHeading,"desc":this.todoDesc})
+      this.todoHeading = "";
+      this.todoDesc = "";
+    }
+  }
+
+  onDelete(delTodo:any) {
+    console.log(delTodo)
+    this.allTodos = this.allTodos.filter((todo:any)=>{return todo.heading !== delTodo.heading})
+  }
+  onDone(checkTodo:any) {
+    console.log();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSliderModule } from '@angular/material/slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,MatButtonModule,FormsModule, MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
