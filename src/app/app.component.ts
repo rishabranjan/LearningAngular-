2beefa679449dd945f1ef77f3c099e0cbc142fda
Rishab Ranjan <therishabranjan@gmail.com>
2022-06-19 23:21:51 +0530

    added error tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,36 +10,56 @@ export class AppComponent {
     {
       heading: 'Make Eggs',
       desc: 'Make omlet from 4 eggs',
+      active: true,
     },
     {
       heading: 'Buy Eggs',
       desc: 'Make omlet from 4 eggs',
+      active: true,
     },
     {
       heading: 'Sell Eggs',
       desc: 'Make omlet from 4 eggs',
+      active: true,
     },
     {
       heading: 'Throw Eggs',
       desc: 'Make omlet from 4 eggs',
+      active: false,
     },
   ];
   todoDesc: string = '';
   todoHeading: string = '';
+  error: boolean = false;
 
   onTodoClick() {
-    if (this.todoHeading!=="" && this.todoDesc!=="") {
-      this.allTodos.push({"heading":this.todoHeading,"desc":this.todoDesc})
-      this.todoHeading = "";
-      this.todoDesc = "";
+    if (this.todoHeading !== '' && this.todoDesc !== '') {
+      this.allTodos.push({
+        heading: this.todoHeading,
+        desc: this.todoDesc,
+        active: true,
+      });
+      this.todoHeading = '';
+      this.todoDesc = '';
+    } else {
+      this.error = true;
     }
   }
 
-  onDelete(delTodo:any) {
-    console.log(delTodo)
-    this.allTodos = this.allTodos.filter((todo:any)=>{return todo.heading !== delTodo.heading})
+  onDelete(delTodo: any) {
+    console.log(delTodo);
+    this.allTodos = this.allTodos.filter((todo: any) => {
+      return todo.heading !== delTodo.heading;
+    });
   }
-  onDone(checkTodo:any) {
-    console.log();
+  onDone(checkTodo: any) {
+    this.allTodos.forEach((element: any) => {
+      if (element.heading === checkTodo.heading) {
+        element.active = !element.active;
+      }
+    });
+  }
+  onKeyInput() {
+    this.error = false;
   }
 }
